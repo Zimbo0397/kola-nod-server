53aa86296fb69642b8ec4f7ece84a204613de3c0
Zimbo0397 <zimbo0397@Gmail.com>
2017-05-26 18:10:08 +0300

    + posrt req

--- server.js
+++ server.js
@@ -28,16 +28,17 @@ var users = [
 	}
 ];
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
+});
 
 
 
-app.post('/users', function(req, res) {
+app.post('/user/login', function(req, res) {
 	res.send(users);
+	console.log(req.body)
 
 });
 
